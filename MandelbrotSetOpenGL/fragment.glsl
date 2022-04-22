39a957d7b1473bba1c14e5b0d6e3edcb01ed956e
darkalit <tkachenkozen@gmail.com>
2022-04-22 22:16:18 +0300

    Add more fractals

--- MandelbrotSetOpenGL/fragment.glsl
+++ MandelbrotSetOpenGL/fragment.glsl
@@ -7,30 +7,161 @@ out vec4 FragColor;
 uniform int maxIterations;
 uniform dvec2 offset;
 uniform double zoom;
+uniform dvec2 jnum;
+uniform int fractalType;
+uniform int coloringType;
 
-dvec2 sqrtIm(dvec2 number){
+vec3 iterNewt(dvec2 coord, int coloring_type);
+vec3 iterBurn(dvec2 coord, int coloring_type);
+vec3 iterMandel(dvec2 coord, int coloring_type);
+vec3 iterJulia(dvec2 coord, dvec2 num, int coloring_type);
+
+void main()
+{
+	if (Coords.x <= 0.005 && Coords.x >= -0.005 &&
+		Coords.y <= 0.005 && Coords.y >= -0.005)
+		FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+	else
+		switch (fractalType)
+		{
+		case 0:
+			FragColor = vec4(iterBurn(dvec2(Coords.x, Coords.y)/zoom - offset, coloringType), 1.0);
+			break;
+		case 1:
+			FragColor = vec4(iterNewt(dvec2(Coords.x, Coords.y)/zoom - offset, coloringType), 1.0);
+			break;
+		case 2:
+			FragColor = vec4(iterJulia(dvec2(Coords.x, Coords.y)/zoom - offset, jnum, coloringType), 1.0);
+			break;
+		case 3:
+			FragColor = vec4(iterMandel(dvec2(Coords.x, Coords.y)/zoom - offset, coloringType), 1.0);
+			break;
+		}
+}
+
+dvec2 sqrIm(dvec2 number)
+{
 	return dvec2(
 		number.x * number.x - number.y * number.y,
 		2 * number.x * number.y
 	);
 }
 
-double iterMandel(dvec2 coord){
-	dvec2 z = dvec2(0, 0);
-	for(int i = 0; i < maxIterations; i++){
-		z = sqrtIm(z) + coord;
-		if (length(z) > 2) 
-			return i / float(maxIterations);
-	}
-	return float(maxIterations);
-}
-
-void main()
+dvec2 cbc(dvec2 number)
 {
-
-	double t = iterMandel(((dvec2(Coords.x, Coords.y))/zoom) - offset);
-	double r = 9.48 * (1.0 - t) * t * t * t;
-//	double g = 16.0 * (1.0 - t) * (1.0 - t) * t * t;
-	double b = 9.48 * (1.0 - t) * (1.0 - t) * (1.0 - t) * t;
-	FragColor = vec4(t-r, t, t-b, 1.0f);
+	return dvec2(
+		number.x * number.x * number.x - 3 * number.x * number.y * number.y,
+		3 * number.x * number.x * number.y - number.y * number.y * number.y
+	);
 }
+
+dvec2 divC(dvec2 n1, dvec2 n2)
+{
+	return dvec2(
+	(n1.x * n2.x + n1.y * n2.y) / (n2.x * n2.x + n2.y * n2.y), 
+	(n1.y * n2.x - n1.x * n2.y) / (n2.x * n2.x + n2.y * n2.y)
+	);
+}
+
+dvec2 mulC(dvec2 n1, dvec2 n2)
+{
+	return dvec2(
+	n1.x * n2.x - n1.y * n2.y,
+	n1.x * n2.y + n1.y * n2.x
+	);
+}
+
+vec3 iterNewt(dvec2 coord, int coloring_type)
+{
+	dvec2 z = coord;
+
+	dvec2 roots[3] =
+	{
+		dvec2(1.0, 0.0), 
+		dvec2(-0.5,  sqrt(3.0)/2.0), 
+		dvec2(-0.5, -sqrt(3.0)/2.0)
+	};
+
+	vec3 color[3] =
+	{
+		vec3(1.0, 0.0, 0.0),
+		vec3(0.0, 1.0, 0.0),
+		vec3(0.0, 0.0, 1.0)
+	};
+
+	for(int i = 0; i < maxIterations; i++)
+	{
+		z = z - mulC(dvec2(1.0, 0.0), divC((cbc(z) - dvec2(1.0, 0.0)), (mulC(dvec2(3.0, 0.0), sqrIm(z)))));
+
+		for (int j = 0; j < 3; j++)
+		{
+			dvec2 diff = z - roots[j];
+			if ((abs(diff.x) < 0.0001) && (abs(diff.y) < 0.0001))
+			{
+				if (coloring_type == 0)
+					return mix(color[j], vec3(0.0), 
+					sqrt((float(i) - log2(log2(float(length(z))))) / float(maxIterations) ) );
+				else if (coloring_type == 1)
+					return mix(color[j], vec3(0.0), 
+					sqrt((float(i) - log2(log2(float(length(diff+1.0))))) / float(maxIterations) ) );
+			}
+		}
+	}
+	return vec3(0.0);
+}
+
+vec3 iterBurn(dvec2 coord, int coloring_type)
+{
+	dvec2 z = dvec2(0, 0);
+
+	for(int i = 0; i < maxIterations; i++)
+	{
+		z = sqrIm(dvec2(abs(z.x), 0.0) + dvec2(0.0, -1.0) * (0.0, abs(z.y))) + coord;
+		if (length(z) > 2)
+			if (coloring_type == 0)
+				return vec3(1.0) - vec3((i % 32 * 8), (i % 32 * 8), (i % 64 * 4)) / 256.0;
+			else if (coloring_type == 1)
+				return mix(vec3(0.0, 0.0, 1.0), vec3(1.0, 1.0, 1.0), 
+				sqrt((float(i) + 1.0 - log2(log2(float(length(z))))) / float(maxIterations) ) );
+	}
+	return vec3(0.0);
+}
+
+vec3 iterMandel(dvec2 coord, int coloring_type)
+{
+	dvec2 z = dvec2(0, 0);
+
+	for(int i = 0; i < maxIterations; i++)
+	{
+		//z = dvec2(z.x, -z.y);
+		z = sqrIm(z) + coord;
+		if (length(z) > 2.0)
+			if (coloring_type == 0)
+				return vec3((i % 4 * 64) / 255.0, (i % 8 * 32) / 255.0, (i % 16 * 16) / 255.0);
+			else if (coloring_type == 1)
+				return mix(vec3(1.0, 0.0, 0.0), vec3(1.0, 1.0, 0.0), 
+				sqrt((float(i) - log2(log2(float(length(z))))) / float(maxIterations) ) );
+	}
+	return vec3(0.0);
+}
+
+
+
+vec3 iterJulia(dvec2 coord, dvec2 num, int coloring_type)
+{
+	dvec2 z = coord;
+
+	for(int i = 0; i < maxIterations; i++)
+	{
+		z = sqrIm(z) + num;
+		//z = sqrIm(dvec2(abs(z.x), 0.0) + dvec2(0.0, -1.0) * (0.0, abs(z.y))) + num;
+		if (length(z) > 2.0)
+			if (coloring_type == 0)
+				return vec3((i % 4 * 64) / 255.0, (i % 8 * 32) / 255.0, (i % 16 * 16) / 255.0);
+			else if (coloring_type == 1)
+				return mix(vec3(1.0, 0.0, 0.0), vec3(1.0, 1.0, 0.0), 
+				sqrt((float(i) - log2(log2(float(length(z))))) / float(maxIterations) ) );
+	}
+	return vec3(0.0);
+}
+
--- MandelbrotSetOpenGL/main.cpp
+++ MandelbrotSetOpenGL/main.cpp
@@ -1,17 +1,21 @@
-#include "IndexBuffer.h"
-#include "VertexBuffer.h"
+#include "headers.h"
 
 void framebuffer_size_callback(GLFWwindow*, int, int);
+void mouseCallback(GLFWwindow*, double, double);
 void process_input(GLFWwindow*);
 void dT();
 void countFPS(std::string& outStr);
-void cnum(std::string& cnum);
+void cnum(std::string& cnum, double, double);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
 
+float lastX = SCR_WIDTH / 2;
+float lastY = SCR_HEIGHT / 2;
+bool mouseF = true;
+
 int num_frames = 0;
-int maxIters = 400;
+int maxIters = 128;
 double last_time = 0.0;
 double lastFrame = 0.0;
 double currentFrame = 0.0;
@@ -19,12 +23,24 @@ double currentFrame = 0.0;
 double delta = 0.0;
 
 double zoom = 0.0;
+double zoom_speed = 10.0;
+
 double offset_x = 0.0;
 double offset_y = 0.0;
+double jnum_x = 0.0;
+double jnum_y = 0.0;
 
 std::string frametime;
-std::string complex_num;
+std::string complex_num = "0.000000+0.000000i";
+std::string cursor_pos = "0.000000+0.000000i";
 
+// from 0 to 3 currently
+// 0 - Burning Ship fractal
+// 1 - Newtons fractal
+// 2 - Julia fractal of Mandelbrot fractal
+// 3 - Mandelbrot fractal
+int fractalType = 2;
+int coloringType = 0; // from 0 to 1 currently
 
 int main()
 {
@@ -48,6 +64,8 @@ int main()
 
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	//glfwSetCursorPos(window, lastX, lastY);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -55,55 +73,64 @@ int main()
 		return -1;
 	}
 
-	// Shader shit
 	Shader shader("vertex.glsl", "fragment.glsl");
-	{
-		double vertices[] = {
-			-1.0f, -1.0f,
-			 1.0f, -1.0f,
-			 1.0f,  1.0f,
+	double vertices[] = {
+		-1.0f, -1.0f,
+		 1.0f, -1.0f,
+		 1.0f,  1.0f,
 
-			 1.0f,  1.0f,
-			-1.0f,  1.0f,
-			-1.0f, -1.0f
-		};
+		 1.0f,  1.0f,
+		-1.0f,  1.0f,
+		-1.0f, -1.0f
+	};
 
-		unsigned int VAO;
-		VertexBuffer VB(&vertices, sizeof(vertices));
+	unsigned int VAO, VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
 
-		glGenVertexArrays(1, &VAO);
-		glBindVertexArray(VAO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
-		glVertexAttribPointer(0, 2, GL_DOUBLE, GL_FALSE, 2 * sizeof(double), (void*)0);
-		glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_DOUBLE, GL_FALSE, 2 * sizeof(double), (void*)0);
+	glEnableVertexAttribArray(0);
 		
+	shader.use();
+	shader.setInt("maxIterations", maxIters);
+	shader.setDouble("zoom", pow(2, zoom));
+	glUniform2d(glGetUniformLocation(shader.ID, "offset"), offset_x, offset_y);
+	shader.setInt("coloringType", coloringType);
+	shader.setInt("fractalType", fractalType);
+		
+	while (!glfwWindowShouldClose(window))
+	{
+		process_input(window);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		dT();
+		countFPS(frametime);
+		cnum(complex_num, offset_x, offset_y);
+		cnum(cursor_pos, 
+			(2.0 * lastX / double(SCR_WIDTH) - 1.0) / pow(2, zoom) - offset_x, 
+			(2.0 * lastY / double(SCR_HEIGHT) - 1.0) / pow(2, zoom) - offset_y);
+		glfwSetWindowTitle(window, ("Fractal / " + frametime + " / " + complex_num + " / " + cursor_pos).c_str());
+		
+		double time = glfwGetTime() / 4.0;		
+
 		shader.use();
-		shader.setInt("maxIterations", maxIters);
 		shader.setDouble("zoom", pow(2, zoom));
 		glUniform2d(glGetUniformLocation(shader.ID, "offset"), offset_x, offset_y);
-		
-		while (!glfwWindowShouldClose(window))
-		{
-			process_input(window);
-			glClear(GL_COLOR_BUFFER_BIT);
+		glUniform2d(glGetUniformLocation(shader.ID, "jnum"), jnum_x, jnum_y);
+		//glUniform2d(glGetUniformLocation(shader.ID, "jnum"), 0.7885 * cos(time), 0.7885 * sin(time));
 
-			dT();
-			countFPS(frametime);
-			cnum(complex_num);
-			glfwSetWindowTitle(window, ("Mandelbrot / " + frametime + " / " + complex_num).c_str());
-		
-			shader.use();
-			shader.setDouble("zoom", pow(2, zoom));
-			glUniform2d(glGetUniformLocation(shader.ID, "offset"), offset_x, offset_y);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-
-			glfwSwapBuffers(window);
-			glfwPollEvents();
-		}
-		glDeleteVertexArrays(1, &VAO);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 	return 0;
@@ -132,9 +159,39 @@ void process_input(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
 		offset_x -= (delta / pow(2, zoom));
 	if (glfwGetKey(window, GLFW_KEY_EQUAL) == GLFW_PRESS)
-		zoom += 2 * delta;
+		zoom += zoom_speed * delta;
 	if (glfwGetKey(window, GLFW_KEY_MINUS) == GLFW_PRESS)
-		zoom -= 10 * delta;
+		zoom -= zoom_speed * delta;
+
+	
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (mouseF)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		mouseF = false;
+	}
+
+	double xoffset = xpos - lastX;
+	double yoffset = lastY - ypos;
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+	{
+		offset_x += xoffset / (double(SCR_WIDTH) * pow(2, zoom)) * 2.0;
+		offset_y += yoffset / (double(SCR_HEIGHT) * pow(2, zoom)) * 2.0;
+	}
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+	{
+		jnum_x = (2.0 * lastX / double(SCR_WIDTH) - 1.0) / pow(2, zoom) - offset_x;
+		jnum_y = - (2.0 * lastY / double(SCR_HEIGHT) - 1.0) / pow(2, zoom) - offset_y;
+	}
+
+	lastX = xpos;
+	lastY = ypos;
 }
 
 void dT()
@@ -152,16 +209,16 @@ void countFPS(std::string& outStr)
 	{
 		outStr =
 			std::to_string(1000.0 / num_frames) + " ms - frame / " +
-			std::to_string(num_frames / (dt)) + " fps";
+			std::to_string(int(num_frames / (dt))) + " fps";
 		num_frames = 0;
 		last_time += 1.0;
 	}
 }
 
-void cnum(std::string &cnum)
+void cnum(std::string &cnum, double x, double y)
 {
-	if (-offset_y > 0)
-		cnum = std::to_string(-offset_x) + "+" + std::to_string(-offset_y) + "i";
-	else if (-offset_y < 0)
-		cnum = std::to_string(-offset_x) + std::to_string(-offset_y) + "i";
+	if (-y > 0)
+		cnum = std::to_string(-x) + "+" + std::to_string(-y) + "i";
+	else if (-y < 0)
+		cnum = std::to_string(-x) + std::to_string(-y) + "i";
 }
